8a44b888519c9060e5e4121ee72fed87bd90e44a
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-12-18 19:27:40 +0500

    commit

--- src/app/header/header-middle/header-middle.component.ts
+++ src/app/header/header-middle/header-middle.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'header-middle',
     templateUrl: './header-middle.component.html',
-    styleUrls: ['./header-middle.component.scss'],
+    styleUrls: [ './header-middle.component.scss' ],
+    imports: [
+        RouterLink,
+        NgOptimizedImage,
+        MatIconModule
+    ],
     standalone: true
 })
 
