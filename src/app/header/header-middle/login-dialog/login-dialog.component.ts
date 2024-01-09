fc5878f80201bcb2bbc603f8e765227c9bef3527
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-01-09 22:47:56 +0500

    commit

--- src/app/header/header-middle/login-dialog/login-dialog.component.ts
+++ src/app/header/header-middle/login-dialog/login-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogClose, MatDialogContent } from '@angular/material/dialog';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
    selector: 'login-dialog',
@@ -9,9 +11,14 @@ import { MatDialogClose, MatDialogContent } from '@angular/material/dialog';
    imports: [
       MatIconModule,
       MatDialogContent,
-      MatDialogClose
+      MatDialogClose,
+      NgxMaskDirective,
+      MatInputModule
    ],
-   standalone: true
+   standalone: true,
+   providers: [
+      provideNgxMask(),
+   ]
 })
 
 export class LoginDialogComponent {
