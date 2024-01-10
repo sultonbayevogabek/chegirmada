1e449e804d0fe80071eccbafdceb365d4b428c03
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-01-11 00:41:54 +0500

    commit

--- src/app/header/header-middle/notifications-button/notifications-button.component.ts
+++ src/app/header/header-middle/notifications-button/notifications-button.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  OverlayComponent
+} from '../../../shared/components/overlay-panel/overlay-panel.component';
 
 @Component({
   selector: 'notifications-button',
   templateUrl: 'notifications-button.component.html',
   styleUrls: [ 'notifications-button.component.scss' ],
   imports: [
-    MatIconModule
+    MatIconModule,
+    OverlayComponent
   ],
   standalone: true
 })
