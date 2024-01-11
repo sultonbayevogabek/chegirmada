909f2ab1e8ae856adb2fe6c5c23561d8586dc878
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-01-11 18:59:28 +0500

    commit

--- src/app/header/header-middle/global-search/global-search.component.ts
+++ src/app/header/header-middle/global-search/global-search.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { NgOptimizedImage } from '@angular/common';
+import { OverlayComponent } from '../../../shared/components/overlay-panel/overlay-panel.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'global-search',
@@ -10,12 +13,45 @@ import { NgOptimizedImage } from '@angular/common';
   imports: [
     MatIconModule,
     MatAutocompleteModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    OverlayComponent,
+    MatTabsModule,
+    MatRippleModule
   ],
   standalone: true,
   encapsulation: ViewEncapsulation.None
 })
 
 export class GlobalSearchComponent {
-  streets: string[] = [ 'Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue', 'Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue' ];
+  streets: string[] = [ 'Champs-Élysées', 'Lombard Street', 'Abbey Road',
+    'Fifth Avenue', 'Champs-Élysées', 'Lombard Street', 'Abbey Road',
+    'Fifth Avenue' ];
+
+  tabIndex = 0;
+  selectedRegion = 'Вся страна';
+  regions = [
+    'Все регионы',
+    'Қорақалпоғистон',
+    'Андижон', 'Бухоро', 'Жиззах',
+    'Қашқадарё', 'Навоий', 'Наманган', 'Самарқанд',
+    'Сурхандарё', 'Сирдарё', 'Тошкент', 'Фарғона',
+    'Хоразм', 'Тошкент шаҳри'
+  ];
+
+  districts = [
+    'Тупроккалъа',
+    'Шовот',
+    'Хонка', 'Урганч', 'Гурлан',
+    'Богот', 'Хазарасп', 'Янгибозор', 'Янгиарик',
+    'Кушкупир', 'Хива',
+  ];
+
+  selectRegion(region: string): void {
+    this.selectedRegion = region;
+    this.tabIndex = 1;
+  }
+
+  backToRegions(): void {
+    this.tabIndex = 0;
+  }
 }
