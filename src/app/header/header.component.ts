2abd4e41b73be06a8484b7b8d19e1a71f234b437
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-01-12 20:00:59 +0500

    commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderTopComponent } from './header-top/header-top.component';
 import { HeaderMiddleComponent } from './header-middle/header-middle.component';
 import { HeaderBottomComponent } from './header-bottom/header-bottom.component';
+import { NgScrollbarModule } from 'ngx-scrollbar';
 
 @Component({
   selector: 'header',
@@ -9,7 +10,8 @@ import { HeaderBottomComponent } from './header-bottom/header-bottom.component';
   imports: [
     HeaderTopComponent,
     HeaderMiddleComponent,
-    HeaderBottomComponent
+    HeaderBottomComponent,
+    NgScrollbarModule
   ],
   standalone: true
 })
