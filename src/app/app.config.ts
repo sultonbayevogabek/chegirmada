9000d2d969ad9bb89e1995c9c29cfd36063a56f3
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-01-29 16:46:43 +0500

    commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideIcons } from './config/icons/icons.provider';
 import { HttpClientModule } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideIcons(),
     importProvidersFrom(
       HttpClientModule
-    )
+    ),
+    { provide: LOCALE_ID, useValue: 'ru-RU' }
   ]
 };
--- src/app/category/category-products-list/category-products-list.component.ts
+++ src/app/category/category-products-list/category-products-list.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-import { MatRippleModule } from '@angular/material/core';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule } from '@angular/forms';
+import { ProductCardComponent } from '../../shared/components/product-card/product-card.component';
 
 @Component({
   selector: 'category-products-list',
@@ -12,12 +16,151 @@ import { MatButtonModule } from '@angular/material/button';
     MatExpansionModule,
     MatIconModule,
     MatRippleModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDatepickerModule, MatNativeDateModule, FormsModule, ProductCardComponent
   ],
   standalone: true
 })
 
 export class CategoryProductsListComponent implements OnInit {
+  startDate = new Date();
+  endDate = new Date();
+
+  products = [
+    {
+      productCardImageUrl: '/assets/products/1.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 124,
+      productCardTitle: 'Фотоаппарат Canon EOS 2000D Kit EF-S 18-55mm III IS Wi-Fi',
+      productCardOldPrice: '4 700 000 сум',
+      productCardNewPrice: '3 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/2.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 214,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '18 500 000 сум',
+      productCardNewPrice: '17 000 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/3.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 356,
+      productCardTitle: 'Наушники Apple AirPods Pro (2-го поколения, 2022), белый',
+      productCardOldPrice: '2 200 000 сум',
+      productCardNewPrice: '1 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/4.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 433,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '1 050 $',
+      productCardNewPrice: '950 $',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/1.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 124,
+      productCardTitle: 'Фотоаппарат Canon EOS 2000D Kit EF-S 18-55mm III IS Wi-Fi',
+      productCardOldPrice: '4 700 000 сум',
+      productCardNewPrice: '3 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/2.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 214,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '18 500 000 сум',
+      productCardNewPrice: '17 000 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/3.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 356,
+      productCardTitle: 'Наушники Apple AirPods Pro (2-го поколения, 2022), белый',
+      productCardOldPrice: '2 200 000 сум',
+      productCardNewPrice: '1 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/4.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 433,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '1 050 $',
+      productCardNewPrice: '950 $',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/1.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 124,
+      productCardTitle: 'Фотоаппарат Canon EOS 2000D Kit EF-S 18-55mm III IS Wi-Fi',
+      productCardOldPrice: '4 700 000 сум',
+      productCardNewPrice: '3 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/2.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 214,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '18 500 000 сум',
+      productCardNewPrice: '17 000 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/3.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 356,
+      productCardTitle: 'Наушники Apple AirPods Pro (2-го поколения, 2022), белый',
+      productCardOldPrice: '2 200 000 сум',
+      productCardNewPrice: '1 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/4.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 433,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '1 050 $',
+      productCardNewPrice: '950 $',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    }
+  ]
 
   ngOnInit() {
 
