b03163ff0a21f270bef40caba28cbf2045bfbd29
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-02-07 00:20:19 +0500

    commit

--- src/app/pages/profile/favourite-companies/favourite-companies.component.ts
+++ src/app/pages/profile/favourite-companies/favourite-companies.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatRipple } from '@angular/material/core';
 import { NgOptimizedImage } from '@angular/common';
+import { ProfileEmptyListComponent } from '../profile-empty-list/profile-empty-list.component';
 
 @Component({
   selector: 'my-information',
@@ -10,7 +11,8 @@ import { NgOptimizedImage } from '@angular/common';
   imports: [
     MatIcon,
     MatRipple,
-    NgOptimizedImage
+    NgOptimizedImage,
+    ProfileEmptyListComponent
   ],
   standalone: true
 })
--- src/app/pages/profile/favourite-products/favourite-products.component.ts
+++ src/app/pages/profile/favourite-products/favourite-products.component.ts
@@ -3,6 +3,7 @@ import { MatIcon } from '@angular/material/icon';
 import { MatRipple } from '@angular/material/core';
 import { NgOptimizedImage } from '@angular/common';
 import { ProfileEmptyListComponent } from '../profile-empty-list/profile-empty-list.component';
+import { ProductCardComponent } from '../../../shared/components/product-card/product-card.component';
 
 @Component({
   selector: 'favourite-products',
@@ -12,9 +13,57 @@ import { ProfileEmptyListComponent } from '../profile-empty-list/profile-empty-l
     MatIcon,
     MatRipple,
     NgOptimizedImage,
-    ProfileEmptyListComponent
+    ProfileEmptyListComponent,
+    ProductCardComponent
   ],
   standalone: true
 })
 
-export class FavouriteProductsComponent {}
+export class FavouriteProductsComponent {
+  products = [
+    {
+      productCardImageUrl: '/assets/products/1.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 124,
+      productCardTitle: 'Фотоаппарат Canon EOS 2000D Kit EF-S 18-55mm III IS Wi-Fi',
+      productCardOldPrice: '4 700 000 сум',
+      productCardNewPrice: '3 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/2.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 214,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '18 500 000 сум',
+      productCardNewPrice: '17 000 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/3.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 356,
+      productCardTitle: 'Наушники Apple AirPods Pro (2-го поколения, 2022), белый',
+      productCardOldPrice: '2 200 000 сум',
+      productCardNewPrice: '1 900 000 сум',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    },
+    {
+      productCardImageUrl: '/assets/products/4.jpg',
+      productCardBrandLogo: '/assets/logos/mac-bro.svg',
+      productCardBrandName: 'MacBro',
+      productCardBrandViews: 433,
+      productCardTitle: 'Смартфон Apple iPhone 13 pro 256 ГБ, Dual: nano SIM + eSIM',
+      productCardOldPrice: '1 050 $',
+      productCardNewPrice: '950 $',
+      productCardLocation: 'Ташкент',
+      productCardDate: '14 но. – 21 дек.',
+    }
+  ]
+}
