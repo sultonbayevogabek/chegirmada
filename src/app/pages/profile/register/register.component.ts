d642c14822cac67b1468b2095fea1820b29ccbe7
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-02-07 20:13:11 +0500

    commit

--- src/app/pages/profile/register/register.component.ts
+++ src/app/pages/profile/register/register.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { MatOption, MatSelect } from '@angular/material/select';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
 
 @Component({
   selector: 'register',
   templateUrl: 'register.component.html',
   styleUrl: 'register.component.scss',
+  imports: [
+    MatSelect,
+    MatOption,
+    NgxMaskDirective,
+    MatRadioGroup,
+    MatRadioButton
+  ],
+  providers: [
+    provideNgxMask()
+  ],
   standalone: true
 })
 
