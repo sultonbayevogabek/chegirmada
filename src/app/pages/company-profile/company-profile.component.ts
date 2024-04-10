6de82a50af53dd5acfb9683214c27f74e07541ab
sultonbayevogabek <sultonbayevogabek@gmail.com>
2024-04-10 14:51:22 +0500

    commit

--- src/app/pages/company-profile/company-profile.component.ts
+++ src/app/pages/company-profile/company-profile.component.ts
@@ -9,6 +9,8 @@ import { YoutubePlayer } from '../../core/components/youtube-player/youtube-play
 import { TabsComponent } from '../../core/components/tabs/tabs.component';
 import { CompanyProfileRateComponent } from './company-profile-rate/company-profile-rate.component';
 import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from '../../core/services/auth.service';
+import { ToasterService } from '../../core/services/toaster.service';
 
 @Component({
   selector: 'company-profile',
@@ -20,35 +22,37 @@ import { MatDialog } from '@angular/material/dialog';
     UiButtonComponent,
     YoutubePlayer,
     TabsComponent,
-    MatRipple,
+    MatRipple
   ],
   standalone: true
 })
 
 export class CompanyProfileComponent implements OnInit {
-  mode: 'static' | 'edit' = 'static'
+  mode: 'static' | 'edit' = 'static';
   tabs = [
     {
-      routerLink: ['discounts'],
+      routerLink: [ 'discounts' ],
       text: 'Скидки'
     },
     {
-      routerLink: ['products'],
+      routerLink: [ 'products' ],
       text: 'Продукты'
     },
     {
-      routerLink: ['branches'],
+      routerLink: [ 'branches' ],
       text: 'Филиали'
     },
     {
-      routerLink: ['expired-discounts'],
+      routerLink: [ 'expired-discounts' ],
       text: 'Законченный скидки'
     }
-  ]
+  ];
 
   private _activatedRoute = inject(ActivatedRoute);
   private _destroyRef = inject(DestroyRef);
+  private _authService = inject(AuthService);
   private _dialog = inject(MatDialog);
+  private _toaster = inject(ToasterService);
 
   ngOnInit(): void {
     this._activatedRoute.queryParams
@@ -57,12 +61,19 @@ export class CompanyProfileComponent implements OnInit {
         if ('mode' in queryParams) {
           this.mode = queryParams['mode'];
         }
-      })
-
-
+      });
   }
 
   openRateModal(): void {
+    if (!this._authService.currentUser) {
+      this._toaster.open({
+        title: 'attention',
+        message: 'you.must.be.authorized.leave.comment',
+        type: 'warning'
+      });
+      return;
+    }
+
     this._dialog.open(CompanyProfileRateComponent, {
       width: '25rem',
       maxWidth: '100%',
@@ -72,6 +83,6 @@ export class CompanyProfileComponent implements OnInit {
         logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUWN4Hx9ybuBWqJkuiE1luauJRdmZqtPsJlAIzTv5ng&s',
         storeId: 3
       }
-    })
+    });
   }
 }
